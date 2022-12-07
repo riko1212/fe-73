c0d770f37f586f5681c103bed66dc324fee81455
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2022-12-07 21:47:24 +0200

    mod-8

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
+    menuList: document.querySelector('.menu-list'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+
+  function removeMenu() {
+    refs.menu.classList.add('is-hidden');
+    refs.body.classList.remove('no-scroll');
+  }
+})();
